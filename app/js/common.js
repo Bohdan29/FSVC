7fb57a6f0d2ad6f11b08d833f4f49a85068a3e2a
Bohdan29 <gitarman@i.ua>
2017-12-23 15:58:20 +0200

    Responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,13 @@
+//mobile menu
+$(document).ready(function(){
+    if($(window).width()<=800) {
+        $(".navbar").hide();
+        $(".hamburger").on("click touch", function(){
+            $(".navbar").slideToggle();
+        });
+    }
+});
+
 //search input
 $('.search-btn').on('click', function(){
 	$('.search-form').toggle();
@@ -11,5 +21,31 @@ $('.single-item').slick({
 
 $('.multiple-items').slick({
 	slidesToShow: 8,
-  slidesToScroll: 8,
+  slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1366,
+      settings: {
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 801,
+      settings: {
+      	slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 481,
+      settings: {
+      	slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 376,
+      settings: {
+      	slidesToShow: 1
+      }
+    }
+  ]
 });
